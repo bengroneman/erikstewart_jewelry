aa9b1e2a09e90a7f5760b3a53d368d6217090a89
bengroneman <bluegroneman10@gmail.com>
2020-08-07 21:37:20 -0600

    switched to srcing tailwind within the build step

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import settings from "../data/theme.json";
 import "@/assets/code-highlight.css"
 import "@/assets/tailwind.css"
 
+// Testing out whether we can source with build step
+// Note when we merge make sure that we end up srcing this way instead of in index.html
+import "@/assets/main-build.css"
+
 export default function(Vue, { head }) {
   Vue.component("Layout", DefaultLayout);
   head.bodyAttrs = {
