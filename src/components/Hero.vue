0a70513086033912d0200143b8748819d31ab63b
bengroneman <bluegroneman10@gmail.com>
2020-08-08 18:57:56 -0600

    updated design wip

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,7 +1,13 @@
 <template>
-    <div class="w-1/2 ml-24">
-        <h1 class="text-5xl" v-html="settings.hero_title" />
-        <h2 class="text-xl" v-html="settings.hero_subtitle" />
+    <div class="w-full">
+      <div class="m-24">
+        <h1 class="text-6xl" v-html="settings.hero_title" />
+        <h2 class="text-xl w-1/3" v-html="settings.hero_subtitle" />
+        <g-link to="/shop" class="text-uppercase bg-black text-white p-2 mt-2 rounded flex inline">
+          View All
+        </g-link>
+        <img src="/uploads/DSC_3030a.jpg" alt="replace this with query data"/>
+      </div>
     </div>
 </template>
 
@@ -14,3 +20,7 @@ export default {
   }
 }
 </script>
+<!-- TODO: find a way to get graphql explore to work offline -->
+<!-- TODO: write a query for the home page and pass the data down to this child component may need to rework some the theme file idea...? --> 
+
+<!-- TODO: Optimize images? Or look into using the gridsome quality parameter for images --> 
