ba01e05c74aaa2e1491b0dc65d5ae75878e39723
bengroneman <bluegroneman10@gmail.com>
2020-08-27 08:13:29 -0600

    WIP tests

--- src/tests/JewelryGrid.spec.js
+++ src/tests/JewelryGrid.spec.js
@@ -1,4 +1,5 @@
 // TODO: find a way to run the latest JS with a transform option
+// TODO: determine why vue-server-render and vue are different version numbers. Should be in the commits
 import JewelryGrid from '@/components/JewelryGrid.vue'
 
 describe('Jewelry grid alternating columns functionality', () => {
