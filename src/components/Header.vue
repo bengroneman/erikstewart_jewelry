b279aa5f5676cd59487e2b121c81cbd69d8c7ee1
bengroneman <bluegroneman10@gmail.com>
2020-09-19 09:30:48 -0600

    tailwind built mobile navigation overhaul

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,10 @@
 <template>
-    <header class="header bg-gray-e z-50" :class="{sticky: $route.path === '/' || $route.path.includes('/shop/')}">
-        <div class="md:ml-24 md:mr-24 flex items-center h-full justify-between">
-            <div class="">
-                <g-link :to="{ name: 'home' }" class="home-link">
+    <header
+        class="bg-gray-e z-40 sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3"
+    >
+        <div class="flex items-center justify-between px-4 py-3 sm:p-0">
+            <div>
+                <g-link :to="{ name: 'home' }">
                     <img 
                         src="../../static/logo-black.png"
                         :alt="settings.site_name" 
@@ -10,19 +12,26 @@
                     />
                 </g-link>
             </div>
-            <nav class="nav">
-                <g-link class="nav__link block md:inline" to="/About">About</g-link>
-                <g-link class="nav__link block md:inline" to="/shop">Jewelry</g-link>
-                <g-link class="nav__link block md:inline" to="https://etsy.com">Etsy Shop</g-link>
-                <a class="nav__link" href="#contact">Say Hi!</a>
-
-                <button type="button" class="text-gray-500 focus:text-white">
+            <div class="sm:hidden">
+                <button
+                    @click="navOpen = !navOpen"
+                    type="button"
+                    class="block text-gray-500 hover:text-white focus:text-white focus:outline-none"
+                >
                     <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
-                        <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
+                        <path v-if="navOpen" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
+                        <path v-if="!navOpen" fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
                     </svg>
                 </button>
-            </nav>
+            </div>
         </div>
+        <nav :class="navOpen ? 'block' : 'hidden'" class="px-2 pt-2 pb-4 sm:flex sm:p-0">
+            <g-link to="/about" class="mt-1 block px-2 py-1 text-dark-gray-e font-semibold hover:text-white hover:bg-gray-800 sm:mt-0 sm:ml-2">About</g-link>
+            <g-link to="/jewelry" class="mt-1 block px-2 py-1 text-dark-gray-e font-semibold hover:text-white hover:bg-gray-800 sm:mt-0 sm:ml-2">Jewelry</g-link>
+            <g-link to="/blog" class="mt-1 block px-2 py-1 text-dark-gray-e font-semibold hover:text-white hover:bg-gray-800 sm:mt-0 sm:ml-2">Blog</g-link>
+            <g-link to="https://etsy.com" class="mt-1 block px-2 py-1 text-dark-gray-e font-semibold hover:text-white hover:bg-gray-800 sm:mt-0 sm:ml-2">Etsy Shop</g-link>
+            <a to="#contact" class="mt-1 block px-2 py-1 text-dark-gray-e font-semibold hover:text-white hover:bg-gray-800 sm:mt-0 sm:ml-2">Contact</a>
+        </nav>
     </header>
 </template>
 
@@ -31,12 +40,27 @@ export default {
   data() {
     return {
         logo: require("../../static/logo-black.png"),
-        settings: require("../../data/theme.json")
+        settings: require("../../data/theme.json"),
+        navOpen: false
     }
   }
 }
 </script>
 <style scoped>
+.open {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    margin: 2rem;
+    justify-content: center;
+}
+.open > a {
+    display: block;
+}
+.closed {
+    display: none;
+}
 .header {
     position: relative;
     height: 6rem;
@@ -63,26 +87,6 @@ export default {
     letter-spacing: 0.05em;
     text-decoration: none;
     text-transform: uppercase;   
-}
-.nav > * {
-    font-size: 0.9rem;
-    font-weight: 600;
-    text-decoration: none;
-    margin-top: 4px;
-    margin-right: 3rem;
-    padding-bottom: 4px;
-    border-bottom: 1px solid;
-    border-color: transparent;
-    transition: border 0.15s;
-}
-.nav > *:last-of-type {
-    margin: 0;
-}
-.nav > *:hover {
-    border-color: inherit;
-}
-.nav > .active {
-    border-color: inherit;
 }
 .bg-gray-theme {
     background: #F5F7F7;
