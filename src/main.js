e250394fe19a65f3d46bdf83909ff5c190f111fb
bengroneman <bluegroneman10@gmail.com>
2020-09-30 19:14:53 -0600

    catching up

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 import DefaultLayout from "~/layouts/Default.vue";
 import settings from "../data/theme.json";
 
+import VueSmoothScroll from 'vue2-smooth-scroll'
 import "@/assets/code-highlight.css"
 import "@/assets/tailwind.css"
 import "@/assets/fonts.css"
 
 import "@/assets/main-build.css"
 
+
 export default function(Vue, { head }) {
   Vue.component("Layout", DefaultLayout);
   head.bodyAttrs = {
     class: settings.dark_mode ? "dark" : ""
   };
+  Vue.use(VueSmoothScroll)
 }
