3f1b2a47bf0280db3883fae72fe0b84f4f9b97f5
bengroneman <bluegroneman10@gmail.com>
2020-11-26 07:34:44 -0700

    sendgrid integration

--- blackbox/submission-created.js
+++ blackbox/submission-created.js
@@ -13,6 +13,12 @@ exports.handler = async event => {
     text: 'and easy to do anywhere, even with Node.js',
     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
   };
-  sgMail.send(msg);
+  sgMail.send(msg)
+    .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  });
 }
 
