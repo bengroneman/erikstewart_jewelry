f080115fee768bf84bd879556a639f3f9b4e0318
bengroneman <bluegroneman10@gmail.com>
2020-11-26 07:56:07 -0700

    development setup for email testing

--- blackbox/submission-created.js
+++ blackbox/submission-created.js
@@ -1,6 +1,9 @@
 // using Twilio SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
 const sgMail = require('@sendgrid/mail');
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.handler = async event => {
