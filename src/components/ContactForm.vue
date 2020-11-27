6775d9a76dfa491b663aeacac9d18d52e7de14fa
bengroneman <bluegroneman10@gmail.com>
2020-11-27 11:52:15 -0700

    feat: manually handling the form submission

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,10 +1,19 @@
 <template>
   <form
     name="contact"
-    method="POST"
     id="contact-form"
+    method="post"
+    @submit.prevent="handleSubmit"
+    action="/success/"
+    data-netlify="true"
+    data-netlify-honeypot="bot-field"
   >
     <input type="hidden" name="contact" value="contact" />
+    <p hidden>
+      <label>
+        Don’t fill this out: <input name="bot-field" />
+      </label>
+    </p>
     <h1 id="contact" class="text-6xl inline-block text-white">Contact</h1>
     <div class="flex flex-wrap -mx-3 mb-6">
       <div class="w-full md:w-1/2 md:pr-6 mb-6 md:mb-0">
@@ -17,6 +26,7 @@
           type="text"
           placeholder="Full Name"
           name="name"
+          v-model="formData.name"
         />
       </div>
         <div class="w-full md:w-1/2">
@@ -29,6 +39,7 @@
             type="text"
             placeholder="jane.doe@hey.com"
             name="emailaddress"
+            v-model="formData.emailaddress"
           />
         </div>
       </div>
@@ -39,8 +50,37 @@
         id="message"
         rows="4"
         name="message"
+        v-model="formData.message"
       ></textarea>
     </div>
-    <input type="submit" class="ml-0 mx-auto text-uppercase bg-blue-gray-e text-white p-2 mt-2 flex pl-6 pr-6 text-light-e" value="submit form"/>
+    <button type="submit" class="ml-0 mx-auto text-uppercase bg-blue-gray-e text-white p-2 mt-2 flex pl-6 pr-6 text-light-e">Submit</button>
   </form>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      formData: {},
+    }
+  },
+  methods: {
+    encode(data) {
+      return Object.keys(data)
+        .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+        .join('&')
+    },
+    handleSubmit(e) {
+      fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: this.encode({
+          'form-name': e.target.getAttribute('name'),
+          ...this.formData,
+        }),
+      })
+      .then(() => this.$router.push('/success'))
+      .catch(error => alert(error))
+    }
+  }
+}
+</script>
