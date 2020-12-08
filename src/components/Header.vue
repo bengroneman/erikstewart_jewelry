24f36ac29f214d3127ece793131351ff18c02a59
Paul Kang <pkang29@gmail.com>
2020-12-08 15:40:47 -0700

    added tailwind improvements

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -42,30 +42,30 @@
     >
       <g-link
         to="/about"
-        class="mt-1 block px-2 py-1 text-dark-gray-e font-semibold hover:text-white hover:bg-gray-800 sm:mt-0 sm:ml-2"
+        class="g-link"
       >
         About
       </g-link>
       <g-link
         to="/shop"
-        class="mt-1 block px-2 py-1 text-dark-gray-e font-semibold hover:text-white hover:bg-gray-800 sm:mt-0 sm:ml-2"
+        class="g-link"
       >
         Jewelry
       </g-link>
       <g-link
         to="/blog"
-        class="mt-1 block px-2 py-1 text-dark-gray-e font-semibold hover:text-white hover:bg-gray-800 sm:mt-0 sm:ml-2"
+        class="g-link"
       >
         Blog
       </g-link>
       <a
         to="https://etsy.com/"
-        class="mt-1 block px-2 py-1 text-dark-gray-e font-semibold hover:text-white hover:bg-gray-800 sm:mt-0 sm:ml-2"
+        class="g-link"
       >Shop</a>
       <a
         v-smooth-scroll
         href="#contact"
-        class="mt-1 block px-2 py-1 text-dark-gray-e font-semibold hover:text-white hover:bg-gray-800 sm:mt-0 sm:ml-2"
+        class="g-link"
       >Contact</a>
     </nav>
   </header>
@@ -83,49 +83,12 @@ export default {
 }
 </script>
 <style scoped>
-.open {
-    height: 100%;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    margin: 2rem;
-    justify-content: center;
-}
-.open > a {
-    display: block;
-}
-.closed {
-    display: none;
-}
-.header {
-    position: relative;
-    height: 6rem;
-    z-index: 10;
-}
-.header.sticky {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-}
-.header > .container {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    height: 100%;
-}
-.home-link {
-    text-decoration: none;
-}
-.site-name {
-    font-size: 0.9rem;
-    font-weight: 700;
-    letter-spacing: 0.05em;
-    text-decoration: none;
-    text-transform: uppercase;   
-}
+
 .bg-gray-theme {
     background: #F5F7F7;
+}
+.g-link { 
+  @apply mt-1 block px-2 py-1 text-dark-gray-e font-semibold hover:text-white hover:bg-gray-800 sm:mt-0 sm:ml-2;
 }
 </style>
 <!-- TODO: dry this code up a bit with tailwind @apply directive -->
