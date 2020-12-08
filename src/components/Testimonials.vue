7031396d07a6c78cf18482e9886a1dbaeee522cc
bengroneman <bluegroneman10@gmail.com>
2020-12-08 14:28:22 -0700

    feat: testimonial component

---
+++ src/components/Testimonials.vue
@@ -0,0 +1,84 @@
+<template>
+  <section>
+    <transition-group
+      name="slide"
+      tag="div"
+    >
+      <div 
+        :id="`testimonial-${i}`"
+        :key="i"
+        v-for="i in [currentIndex]"
+        class="w-full flex bg-light-gray-e justify-center p-12 pb-24"
+      >
+        <div class="w-2/3 text-4xl">
+          <span class="block text-6xl text-dark-gray-e relative -left-2 top-2">"</span>
+          {{ currentTestimonial.node.body }}
+          <span class="">"</span>
+          <span class="block mt-3">- {{ currentTestimonial.node.author }}</span>
+          <div class="flex justify-around mt-12">
+            <a
+              class="prev"
+              href="#"
+              @click="prev"
+            >&#10094;</a>
+            <a
+              class=""
+              href="#"
+              @click="next"
+            >&#10095;</a>
+          </div>
+        </div>
+      </div>
+    </transition-group>
+  </section>
+</template>
+
+<static-query>
+query Testimonials {
+  testimonials: allTestimonials(sortBy: "author") {
+    edges {
+      node {
+        id
+        body
+        author
+      }
+    }
+  }
+}
+</static-query>
+
+<script>
+export default {
+  data() {
+    return {
+      timer: null,
+      currentIndex: 0,
+    }
+  },
+
+  computed: {
+    currentTestimonial: function() {
+      const testimonials = this.$static.testimonials.edges
+      return testimonials[Math.abs(this.currentIndex) % testimonials.length];
+    }
+  },
+
+  mounted() {
+    this.startSlide()
+  },
+
+  methods: {
+    startSlide: function() {
+      this.timer = setInterval(this.next, 4000)
+    },
+
+    next: function() {
+      this.currentIndex += 1
+    },
+
+    prev: function() {
+      this.currentIndex -= 1
+    }
+  },
+}
+</script>
