629246e61145920a9f42dd555978e9144f4702f6
bengroneman <bluegroneman10@gmail.com>
2020-12-13 08:46:59 -0700

    feat: WIP about page rework

---
+++ src/components/TwoColumnSection.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="w-full md:pl-40 flex flex-row">
+    <div class="m-12 w-1/2">
+      <slot name="leftcolumn">
+        Add some content
+      </slot>
+    </div>
+    <div class="m-12 flex w-1/2 justify-center" >
+      <slot name="rightcolumn">
+        Add some content
+      </slot>
+    </div>
+  </section>
+</template>
